169d0aa970e7e67323ba9433e225ae566cd73eb2
Balatamoghna <bala98410@gmail.com>
2021-08-07 16:11:57 +0530

    Project Submission

---
+++ src/main/java/com/externship/appointment/PersonRepository.java
@@ -0,0 +1,8 @@
+package com.externship.appointment;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person,String>{
+	
+	
+}
